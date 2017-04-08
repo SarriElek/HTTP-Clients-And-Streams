784362c7cab57b6ca4fb656ec4e80a7f58c6713e
Edurne Berastegi Zabalza <edurne.berastegi@gmail.com>
2017-04-08 22:51:46 +0000

    add a stream to write the data into

--- https_example.js
+++ https_example.js
@@ -1,17 +1,26 @@
 var https = require("https");
+var fs = require("fs");
 
 var options = {
   host: 'stream-large-file.herokuapp.com',
   path: '/give-me-stuff-now'
 };
 
+var data = "";
+
 // called by https when the request is made.
 var callback = function(response) {
   console.log('In response handler callback!');
   // Response is a stream
   response.on('data', function(chunk) {
-    console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
-    console.log(chunk.toString());
+    //console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+    //console.log(chunk.toString());
+    data += chunk;
+  });
+  response.on("end", function(){
+    var writerStream = fs.createWriteStream('output.txt');
+    writerStream.write(data,'UTF8');
+    writerStream.end();
   });
 }
 console.log("I'm about to make the request!");
