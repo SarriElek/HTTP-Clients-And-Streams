812ce5e120841882927c91a911b8ff0d704f3b33
Edurne Berastegi Zabalza <edurne.berastegi@gmail.com>
2017-04-08 22:40:13 +0000

    added onData event for response stream

--- https_example.js
+++ https_example.js
@@ -8,7 +8,11 @@ var options = {
 // called by https when the request is made.
 var callback = function(response) {
   console.log('In response handler callback!');
-  console.log('Response: ', response);
+  // Response is a stream
+  response.on('data', function(chunk) {
+    console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+    console.log(chunk.toString());
+  });
 }
 console.log("I'm about to make the request!");
 
